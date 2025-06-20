2dd5ed07fcd0d36e7d06f5196255d45be7c723f9
Andreas Raja Goklas Sitorus <andreasrajagoklassitorus@gmail.com>
2025-06-20 23:39:56 +0200

    feat: Add sac trainer script.

--- ast_sac/torch/core/torch_rl_algorithm.py
+++ ast_sac/torch/core/torch_rl_algorithm.py
@@ -4,7 +4,50 @@ from collections import OrderedDict
 from typing import Iterable
 from torch import nn
 
-# from ast_sac.core.batch_rl_algorithm import BatchRLAgorithm
-# from ast_sac.core.online_rl_algorithm import OnlineRLAlgorithm
-# from ast_sac.core.trainer import Trainer
+from ast_sac.core.batch_rl_algorithm import BatchRLAlgorithm
+from ast_sac.core.online_rl_algorithm import OnlineRLAlgorithm
+from ast_sac.core.trainer import Trainer
 from ast_sac.torch.core.module import np_to_pytorch_batch
+
+class TorchOnlineRLAlgorithm(OnlineRLAlgorithm):
+    def to(self, device):
+        for net in self.trainer.networks:
+            net.to(device)
+
+    def training_mode(self, mode):
+        for net in self.trainer.networks:
+            net.train(mode)
+
+
+class TorchBatchRLAlgorithm(BatchRLAlgorithm):
+    def to(self, device):
+        for net in self.trainer.networks:
+            net.to(device)
+
+    def training_mode(self, mode):
+        for net in self.trainer.networks:
+            net.train(mode)
+
+
+class TorchTrainer(Trainer, metaclass=abc.ABCMeta):
+    def __init__(self):
+        self._num_train_steps = 0
+
+    def train(self, np_batch):
+        self._num_train_steps += 1
+        batch = np_to_pytorch_batch(np_batch)
+        self.train_from_torch(batch)
+
+    def get_diagnostics(self):
+        return OrderedDict([
+            ('num train calls', self._num_train_steps),
+        ])
+
+    @abc.abstractmethod
+    def train_from_torch(self, batch):
+        pass
+
+    @property
+    @abc.abstractmethod
+    def networks(self) -> Iterable[nn.Module]:
+        pass
